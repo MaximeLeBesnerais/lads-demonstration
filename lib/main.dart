2b991dc4d64f389020f807b3faff937fe64ec23b
Maxime <maxoulebesnerais@epitech.eu>
2025-04-21 15:40:47 +0800

    Refactor main function to support command-line arguments for GUI and CLI execution

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,33 @@
-import 'package:flutter/material.dart';
+import 'package:args/args.dart';
+import 'package:lads/core/command_processor.dart';
+import 'package:lads/entry/gui.dart';
+import 'package:lads/entry/repl.dart';
+import 'package:lads/model/orchestrator.dart';
+import 'package:lads/services/ai.dart';
+import 'package:lads/services/cli_service.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main(List<String> args) {
+  GeminiService geminiService = GeminiService();
+  Orchestrator orchestrator = Orchestrator();
+  CommandProcessor commandProcessor = CommandProcessor(orchestrator, geminiService);
+  CliService cliService = CliService(commandProcessor);
+  final parser = ArgParser()
+    ..addFlag('gui', abbr: 'g', help: 'Run the GUI')
+    ..addFlag('cli', abbr: 'c', help: 'Run the REPL')
+    ..addFlag('help', abbr: 'h', help: 'Show this help message');
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final argResults = parser.parse(args);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  if (argResults['help'] as bool) {
+    print('Usage: lads [options]');
+    print(parser.usage);
+    return;
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+  if (argResults['gui'] as bool) {
+    runGui();
+  } else if (argResults['cli'] as bool) {
+    runCliRepl(cliService);
+  } else {
+    print('No option specified. Use --help for usage information.');
   }
 }
