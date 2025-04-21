71a21dc11afcdc4de21ac56495d26b8c0ddfc891
Maxime <maxoulebesnerais@epitech.eu>
2025-04-21 15:35:59 +0800

    Refactor CommandResult and AiCommandResult classes for improved clarity and structure

--- lib/model/command_result.dart
+++ lib/model/command_result.dart
@@ -1,23 +1,32 @@
-import '../core/command_processor.dart';
+class AiCommandResult {
+  final String? message;
+  final List<String>? instructions;
+  final String answerType;
+
+  AiCommandResult({
+    required this.answerType,
+    this.message,
+    this.instructions,
+  });
+}
 
 enum CommandResultType {
   nodeList,
   taskList,
   nodeStatus,
   logList,
-  aiResponse, // Contains AiCommandResult
+  aiResponse,
   simpleMessage,
   error
 }
 
 class CommandResult {
   final CommandResultType type;
-  final dynamic data; // Can be List<Node>, List<Task>, Node?, List<String>, AiCommandResult, String
-  final String? errorMessage; // Used when type is error
+  final dynamic data;
+  final String? errorMessage;
 
   CommandResult({required this.type, this.data, this.errorMessage});
 
-  // Factory constructor for convenience
   factory CommandResult.success(CommandResultType type, dynamic data) {
     return CommandResult(type: type, data: data);
   }
@@ -26,9 +35,9 @@ class CommandResult {
     return CommandResult(type: CommandResultType.simpleMessage, data: message);
   }
 
-   factory CommandResult.ai(AiCommandResult aiData) {
-     return CommandResult(type: CommandResultType.aiResponse, data: aiData);
-   }
+  factory CommandResult.ai(AiCommandResult aiData) {
+    return CommandResult(type: CommandResultType.aiResponse, data: aiData);
+  }
 
   factory CommandResult.error(String message) {
     return CommandResult(type: CommandResultType.error, errorMessage: message);
